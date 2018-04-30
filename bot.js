0292cc17167949224a5a10472e30b2ddbf352d15
RealSparky2000 <38855749+RealSparky2000@users.noreply.github.com>
2018-04-30 15:43:34 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,34 +9,33 @@ client.on('ready', () => {
 var PREFIX = '&'
 
 client.on('message', message => {    
-	//чтобы бот не реагировал если сообщение не начинается с префикса
+    //чтобы бот не реагировал если сообщение не начинается с префикса
   if (!message.content.startsWith(PREFIX) || message.content.startsWith(PREFIX + PREFIX)) return
    var args = message.content.substring(PREFIX.length).split(" ");
   //метод
-	   switch (args[0].toLowerCase()) {
+       switch (args[0].toLowerCase()) {
 
 
-	   case "ping":
-	   message.channel.send('PONG!');
-	   break;
+       case "ping":
+       message.channel.send('PONG!');
+       break;
 
-	   case "bing":
-	  message.channel.send('BONG!');
-	  break;
+       case "bing":
+      message.channel.send('BONG!');
+      break;
 
    case "how-are-you":
-	  message.channel.send('NICE!');
-	   break;
+      message.channel.send('NICE!');
+       break;
 
-	  case "embed":
-	   var embed = new Discord.RichEmbed()
-	   .setAuthor('Заголовок с иконкой', client.user.avatarURL)
-	   .setTitle('Второй заголовок без иконки')
-	   .setDescription('Текст **внутри**')
-	   .setImage(message.author.avatarURL)
-	   .setColor(`0x${args[1]}`)
-	   message.channel.send(embed)
-	   break;
+      case "embed":
+       var embed = new Discord.RichEmbed()
+       .setAuthor('Заголовок с иконкой', client.user.avatarURL)
+       .setTitle('Второй заголовок без иконки')
+       .setDescription('Текст **внутри**')
+       .setImage(message.author.avatarURL)
+       .setColor(`0x${args[1]}`)
+       message.channel.send(embed)
+       break;
+       
  }})
-
-  client.login(process.env.BOT_TOKEN);
