72e68229fd5098ec1127a58c3b7c9ebd25d5aba5
s0undL <36993107+s0undL@users.noreply.github.com>
2018-04-30 15:08:49 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,23 +7,27 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
-var prefix = '&'
+var PREFIX = '&'
 
 client.on('message', message => {
-    
     //чтобы бот не реагировал если сообщение не начинается с префикса
-    if(!message.content.startsWith(prefix)) return
+    if (!message.content.startsWith(PREFIX) || message.content.startsWith(PREFIX + PREFIX)) return
+    var args = message.content.substring(PREFIX.length).split(" ");
+    //метод
+		switch (args[0].toLowerCase()) {
 
-    if (message.content === prefix+'ping') {
+        case "ping":
         message.channel.send('PONG!');
-      }
-    if (message.content === prefix+'bing') {
-        message.reply('BONG!');
-      }
-    if (message.content === prefix+'how are you') {
-        message.reply('NICE!');
-     }
-});
+        break;
 
+        case "bing":
+        message.channel.send('BONG!');
+        break;
+
+        case "how are you":
+        message.channel.send('NICE!');
+        break;
+
+        }})
 
 client.login(process.env.BOT_TOKEN);
