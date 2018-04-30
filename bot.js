6de44ac1f048d5066918ff6cb75361a0106603f7
RealSparky2000 <38855749+RealSparky2000@users.noreply.github.com>
2018-04-30 14:45:59 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,8 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+if(message.content.startsWith(prefix)) return
+
 client.on('ready', () => {
     console.log('I am ready!');
 });
