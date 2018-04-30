72fdde2ef43745a79920d53788aaf0ce093920e6
RealSparky2000 <38855749+RealSparky2000@users.noreply.github.com>
2018-04-30 15:11:55 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,33 +1,46 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-
-//if(message.content.startsWith(prefix)) return
-
-client.on('ready', () => {
-    console.log('I am ready!');
-});
-
-var PREFIX = '&'
-
-client.on('message', message => {
-    //чтобы бот не реагировал если сообщение не начинается с префикса
-    if (!message.content.startsWith(PREFIX) || message.content.startsWith(PREFIX + PREFIX)) return
-    var args = message.content.substring(PREFIX.length).split(" ");
-    //метод
-		switch (args[0].toLowerCase()) {
-
-        case "ping":
-        message.channel.send('PONG!');
-        break;
-
-        case "bing":
-        message.channel.send('BONG!');
-        break;
-
-        case "how are you":
-        message.channel.send('NICE!');
-        break;
-
-        }})
-
-client.login(process.env.BOT_TOKEN);
+ const Discord = require('discord.js');
+ const client = new Discord.Client();
+ 
+-if(message.content.startsWith(prefix)) return
++//if(message.content.startsWith(prefix)) return
+ 
+ client.on('ready', () => {
+     console.log('I am ready!');
+ });
+ 
+-var prefix = '&'
++var PREFIX = '&'
+ 
+ client.on('message', message => {
+-    
+     //чтобы бот не реагировал если сообщение не начинается с префикса
+-    if(!message.content.startsWith(prefix)) return
++    if (!message.content.startsWith(PREFIX) || message.content.startsWith(PREFIX + PREFIX)) return
++    var args = message.content.substring(PREFIX.length).split(" ");
++    //метод
++		switch (args[0].toLowerCase()) {
+ 
+-    if (message.content === prefix+'ping') {
++        case "ping":
+         message.channel.send('PONG!');
+-      }
+-    if (message.content === prefix+'bing') {
+-        message.reply('BONG!');
+-      }
+-    if (message.content === prefix+'how are you') {
+-        message.reply('NICE!');
+-     }
+-});
++        break;
++
++        case "bing":
++        message.channel.send('BONG!');
++        break;
++
++        case "how are you":
++        message.channel.send('NICE!');
++        break;
+ 
++        }})
+ 
+ client.login(process.env.BOT_TOKEN);
